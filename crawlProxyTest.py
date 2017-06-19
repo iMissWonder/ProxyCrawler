dcda1a17cf70b452fbe4a31b86c706327ac8d43e
iMissWonder <joker528@icloud.com>
2017-06-19 18:13:41 +0800

    0.3

--- crawlProxyTest.py
+++ crawlProxyTest.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from scrapy.spiders import Spider
 #from scrapy import cmdline
 #cmdline.execute("scrapy crawl ProxyTestSpider".split())
-
+from ProxyCrawler.spiders import ProxyTestSpider
 from scrapy.crawler import CrawlerRunner
 runner = CrawlerRunner()
 
-runner.create_crawler("ProxyTestSpider")
-runner.create_crawler("ProxyTestSpider")
+runner.crawl("ProxyTestSpider")
+#runner.create_crawler("ProxyTestSpider")
